0f4b99044cbdf5a90ff2604cfb0cd37a1c8988ec
Jaime Abbariao <jaimeabbariao@gmail.com>
2021-10-20 10:00:05 -0400

    simplifying input arguments no need for enums yet

--- reddit/src/subreddit/mod.rs
+++ reddit/src/subreddit/mod.rs
@@ -4,8 +4,6 @@ mod constants;
 mod errors;
 mod utils;
 
-use constants::{Frequency, Index};
-use errors::RedditQueryError;
 use utils::{add_query_config_options, build_subreddit_url_by_index};
 
 pub struct RedditSubreddit {
@@ -20,23 +18,17 @@ impl RedditSubreddit {
     pub async fn query(
         &self,
         subreddit: &str,
-        index: &Index,
-        frequency: &Frequency,
+        index: &str,
+        frequency: &str,
         limit: u32,
-    ) -> Result<String, RedditQueryError> {
-        let url: String =
-            build_subreddit_url_by_index(subreddit, index).unwrap_or(String::from(""));
-        let url_with_options: String =
-            add_query_config_options(url.as_str(), limit, frequency).unwrap_or(String::from(""));
+    ) -> Result<String, ()> {
+        let url: String = build_subreddit_url_by_index(subreddit, index);
+        let url_with_options: String = add_query_config_options(url, frequency, limit);
 
-        if url_with_options.is_empty() {
-            return Err(RedditQueryError::MalformedUrlError(String::from(
-                "Unable to create URL for query",
-            )));
-        }
+        let result = self.client.get(url_with_options).send().await?;
 
-        // TODO: Need to make sure we create the proper
-        // response object for this
-        Ok(self.client.get(url_with_options).send().await?);
+        println!("{:?}", result);
+
+        Ok(String::from("Something"))
     }
 }
